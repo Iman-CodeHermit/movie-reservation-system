0011b7d7d91f46e040b76f04616a0be12d67a9f4
Iman <imanmozafari.neonser@gmail.com>
2025-04-11 21:49:11 +0330

    create custom model user and user manager

---
+++ accounts/managers.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def creat_user(self, phone_number, email, password, full_name):
+        if not phone_number:
+            raise ValueError('user must have phone number')
+
+        if not email:
+            raise ValueError('user must have email')
+
+        if not full_name:
+            raise ValueError('user must have full name')
+
+        user = self.model(phone_number=phone_number,
+                          email=self.normalize_email(email),
+                          full_name=full_name)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+    def create_superuser(self, phone_number, email, password, full_name):
+        user = self.creat_user(phone_number, email, password, full_name)
+        user.is_admin = True
+        user.save(useing=self.db)
+        return user
