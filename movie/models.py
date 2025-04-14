3ffc9a1593fd9def5ea1a8ef2b9c15cf445a3ec6
Iman <imanmozafari.neonser@gmail.com>
2025-04-15 02:06:28 +0330

    add Director and aactor, movie, comment model

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+
+class Director(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Actor(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    summary = models.TextField()
+    director = models.ForeignKey(Director, on_delete=models.SET_NULL, null=True)
+    actors = models.ForeignKey(Actor)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    text = models.TextField()
+    rating = models.IntegerField(max_length=5)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.full_name} - {self.movie.title} - {self.rating}"
