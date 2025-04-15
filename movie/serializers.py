72c4787a17d5f39014a996c9eeaca68a9aa2e278
Iman <imanmozafari.neonser@gmail.com>
2025-04-16 00:31:15 +0330

    add CommenttSerialzer

--- movie/serializers.py
+++ movie/serializers.py
@@ -1,1 +1,32 @@
+from rest_framework import serializers
+from models import Comment
+from reservation.models import Ticket
 
+
+class CommentSerializer(serializers.ModelSerializer):
+    movie_title = serializers.CharField(source='movie.title', read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'movie', 'movie_title', 'text', 'rating', 'created_at']
+        extra_kwargs = {
+            'movie' : {'write_only':True},
+            'text' : {'required':True},
+            'rating' : {'required':True},
+        }
+
+        def validate_rating(self, value):
+            if value < 1 or value > 5:
+                return serializers.ValidationError('rating must be between 1 and 5')
+            return value
+
+        def validate(self, data):
+            user = self.context['request'].user
+            movie = data['movie']
+            if not Ticket.objects.filter(user=user, movie=movie).exists():
+                return serializers.ValidationError('you do not have access add comment for this movie because dont reserved')
+            return data
+
+        def create(self, validated_data):
+            user = self.context['request'].user
+            return Comment.objects.create(user=user, **validated_data)
