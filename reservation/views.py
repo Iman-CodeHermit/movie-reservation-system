c2a4e1b403ac16a5e0e9ccdef518aa54034712c8
Iman <imanmozafari.neonser@gmail.com>
2025-04-16 00:56:10 +0330

    add ReservationTicketView

--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from serializers import TicketSerializer
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+class ReservationTicketView(APIView):
+    permission_classes = [IsAuthenticated,]
+
+    def post(self, request):
+        serializer = TicketSerializer(data=request.data, ccontext={'request':request})
+        if serializer.is_valid():
+            ticket = serializer.save()
+            return Response(TicketSerializer(ticket).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
