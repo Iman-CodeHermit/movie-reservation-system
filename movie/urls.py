ed454ef78ea2c3193e5edb9a93186fafc7b724c2
Iman <imanmozafari.neonser@gmail.com>
2025-04-19 18:46:16 +0330

    add MovieDetailView

--- movie/urls.py
+++ movie/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'movie'
 urlpatterns = [
+    path('movie/<int:pk>/', views.MovieDetailView.as_view(), name='movie-detail'),
     path('comment/', views.CreateCommentView.as_view(), name= 'comment'),
 ]
--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,21 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import CommentSerializer
+from .serializers import CommentSerializer, MovieSerializer
 from rest_framework import status
+from .models import Movie
 from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+class MovieDetailView(APIView):
+    def get(self, request, pk):
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except movie.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 class CreateCommentView(APIView):
     permission_classes = [IsAuthenticated,]
 
