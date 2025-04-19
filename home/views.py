2de8f05d82a3c5b442c4208b807cd65c5bddbb4a
Iman <imanmozafari.neonser@gmail.com>
2025-04-19 17:33:43 +0330

    change serializer name

--- home/views.py
+++ home/views.py
@@ -2,10 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from movie.models import Movie
-from .serializers import HomePageSerializer
+from .serializers import AvailableMoviesSerializer
 
 class AvailableMoviesView(APIView):
     def get(self, request):
         movies = Movie.objects.filter(sold_out=False)
-        serializer = HomePageSerializer(movies, many=True)
+        serializer = AvailableMoviesSerializer(movies, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
