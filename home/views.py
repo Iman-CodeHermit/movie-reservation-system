9951325be65a8eca4eaf9933053b370881d509f5
Iman <imanmozafari.neonser@gmail.com>
2025-05-05 20:03:51 +0330

    add search query for search to movie in AvailableMoviesView

--- home/views.py
+++ home/views.py
@@ -6,6 +6,9 @@ from .serializers import AvailableMoviesSerializer
 
 class AvailableMoviesView(APIView):
     def get(self, request):
+        search_query = request.query_params.get('search', '').strip()
         movies = Movie.objects.filter(sold_out=False)
+        if search_query:
+            movies = movies.filter(title__icontains=search_query)
         serializer = AvailableMoviesSerializer(movies, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
