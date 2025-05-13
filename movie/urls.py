2b2414cb1c2b19bcf84eec83831e462108c5302d
Iman <imanmozafari.neonser@gmail.com>
2025-05-13 03:50:10 +0330

    change LikeMovieView url

--- movie/urls.py
+++ movie/urls.py
@@ -5,5 +5,5 @@ app_name = 'movie'
 urlpatterns = [
     path('<int:pk>/', views.MovieDetailView.as_view(), name='movie-detail'),
     path('comment/', views.CreateCommentView.as_view(), name= 'comment'),
-    path('movies/<int:pk>/like/', views.LikeMovieView.as_view(), name='movie-like'),
+    path('<int:pk>/like/', views.LikeMovieView.as_view(), name='movie-like'),
 ]
